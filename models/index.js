0bd217f0fd106ed46319cfa53e196646e6e63ea3
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-06-08 17:43:27 -0400

    added index.js in models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Imports user and thought models from files
+const user = require('./user');
+const thought = require('./thought');
+
+// Exports user and thought models
+module.exports = { user, thought };
